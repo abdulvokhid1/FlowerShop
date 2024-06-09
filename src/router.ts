51eb21c3e3dd324ec1f665a43dc0c4a39d0f065f
ali <ibrakhimov7747a@gmail.com>
2024-06-10 04:53:27 +0900

    feat: orginize spa & ssr business logic

--- src/router.ts
+++ src/router.ts
@@ -3,4 +3,7 @@ import memberController from "./controllers/member.controller";
 
 const router = express.Router();
 
+router.post("/login", memberController.login);
+router.post("/signup", memberController.signup);
+
 export default router;
